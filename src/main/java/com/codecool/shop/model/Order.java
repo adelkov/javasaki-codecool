792020034dc7e2d0f18ac0a660bbb719e613c15c
Adel Kovacs <kovacsadel12@gmail.com>
2018-04-17 15:20:14 +0200

    cart route

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Multiset;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Order {
     private Multiset<Product> cartProducts = HashMultiset.create();
@@ -18,26 +19,30 @@ public class Order {
     }
 
     public void addOneProduct(Product product){
-        cartProducts.add(product, 1);
+        cartProducts.add(product);
     }
 
     public void removeOneProduct(Product product){
-        cartProducts.remove(product, 1);
+        cartProducts.remove(product);
     }
 
     public void removeProduct(Product product){
-        cartProducts.remove(product);
+        cartProducts.remove(product, cartProducts.count(product));
     }
 
     public int getProductCount(Product product){
         return cartProducts.count(product);
     }
 
-    public float getTotalPrice(){
+    public String getTotalPrice(){
         float totalPrice = 0;
         for (Product product:cartProducts.elementSet()){
             totalPrice += product.getDefaultPrice() * cartProducts.count(product);
         }
-        return totalPrice;
+        return totalPrice + " USD";
+    }
+
+    public Set<Product> getProducts(){
+        return cartProducts.elementSet();
     }
 }
