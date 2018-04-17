81a384fbae6b024af7084e3f2c5af3ee73163248
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-04-17 15:41:44 +0200

    Controller and html created for payment phase

---
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -0,0 +1,40 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/payment"})
+public class PaymentController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        engine.process("payment.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String paymentMethod = req.getParameter("paymentMethod");
+        if(paymentMethod.equals("Card")){
+            // TODO
+        }else if(paymentMethod.equals("PayPal")){
+            // TODO
+        }
+    }
+
+    /*protected void handleCard(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        engine.process("payment.html", context, resp.getWriter());
+    }*/
+}
