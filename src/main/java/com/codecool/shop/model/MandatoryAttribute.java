4b3cf91c9ba8352f7f5f99f88f27ca29b9b4c9f4
Alex <lisztes.alex@gmail.com>
2018-04-18 14:29:25 +0200

    Adding mandatory attribute class
    When you create a mandatoryAttribute instance for a product category, you can set which fields you want as mandatory so we can compare items within the same category

---
+++ src/main/java/com/codecool/shop/model/MandatoryAttribute.java
@@ -0,0 +1,24 @@
+package com.codecool.shop.model;
+
+public class MandatoryAttribute {
+    public enum allAttributes{
+        WEIGHT,
+        LENGTH,
+        COLOR,
+        DIAMETER,
+        MATERIAL,
+        TYPE,
+        HP,
+        EXHAUST_TUBE_NUMBER
+    }
+
+    private allAttributes[] mandatory;
+
+    public MandatoryAttribute(allAttributes... params){
+        mandatory = params;
+    }
+
+    public allAttributes[] getAttributes(){
+        return this.mandatory;
+    }
+}
