e9941843f57cb7454ba124362c9d7bd300ddd207
Alex <lisztes.alex@gmail.com>
2018-04-18 14:32:48 +0200

    New factory which gives you mandatoryAttribute instances
    You can ask this factory for mandatoryAttribute instances for each category so you don't have to mess with the Attribute constructor, also makes them uniform

---
+++ src/main/java/com/codecool/shop/model/AttributeFactory.java
@@ -0,0 +1,16 @@
+package com.codecool.shop.model;
+
+import com.codecool.shop.model.MandatoryAttribute.allAttributes;
+
+public class AttributeFactory {
+
+    public static MandatoryAttribute newInstanceOfEngineAttribute() {
+        return new MandatoryAttribute(allAttributes.HP, allAttributes.EXHAUST_TUBE_NUMBER, allAttributes.WEIGHT, allAttributes.COLOR);
+    }
+
+    public static MandatoryAttribute newInstanceOfWheelAttribute() {
+        return new MandatoryAttribute(allAttributes.DIAMETER, allAttributes.TYPE, allAttributes.MATERIAL, allAttributes.COLOR);
+    }
+
+    // TODO: Finish all categories
+}
