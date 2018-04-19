b8b986554e3564ed2548b0cf0bc5d011dc1467a3
Adel Kovacs <kovacsadel12@gmail.com>
2018-04-19 11:03:11 +0200

    cart delete button control in js

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     var cartLabel = document.getElementById("cartLabel");
+    var cartButton = document.getElementById("shopping-cart");
 
     if (cartLabel.getAttribute("data") === "0") {
         cartLabel.style.visibility = "hidden";
+        cartButton.style.visibility = "hidden";
     } else {
         cartLabel.style.visibility = "visible";
+        cartButton.style.visibility = "visible";
     }
 });
