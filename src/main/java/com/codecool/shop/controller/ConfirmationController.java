79a1841eb60b205c724222b1960908a1f79885ef
peterBarazutti <barazutti.peter@gmail.com>
2018-04-19 19:17:45 +0200

    Fixed: submit button no longer resides in the footer Added design to header

--- src/main/java/com/codecool/shop/controller/ConfirmationController.java
+++ src/main/java/com/codecool/shop/controller/ConfirmationController.java
@@ -37,6 +37,7 @@ public class ConfirmationController extends HttpServlet {
         context.setVariable("zip", order.getZipBilling());
         context.setVariable("city", order.getCityBilling());
         context.setVariable("address", order.getAddressBilling());
+        context.setVariable("pageName", "Confirmation");
 
         engine.process("product/confirmation.html", context, resp.getWriter());
     }
