f3b9d41207663b1ae5e19b84ad3227e67de12f5f
Adel Kovacs <kovacsadel12@gmail.com>
2018-05-02 15:20:06 +0200

    add query string

--- src/main/java/com/codecool/shop/dao/implementation/UserDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoJdbc.java
@@ -1,16 +1,25 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.model.User;
+import com.codecool.shop.dao.implementation.DBConnector;
 
+
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.List;
 
 public class UserDaoJdbc {
     void add(User user){
-
+        String query ="INSERT INTO users (name, email, hash_password)" +
+                "VALUES (?,?,?)";
+        String[] params = {user.getName(), user.getEmail(), user.getPassword()};
+        DBConnector.getInstance().executeQuery(query, params);
     }
 
     User find(int id){
-
+        String query ="";
+        String[] params = {};
+        ResultSet res = DBConnector.getInstance().executeSelectQuery(query, params);
         return null;
     }
 
