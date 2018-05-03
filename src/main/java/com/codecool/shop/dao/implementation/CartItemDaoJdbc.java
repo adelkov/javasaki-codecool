ce2e357a1c7230f07ced3e7db6ec8cd07b6f89ef
peterBarazutti <barazutti.peter@gmail.com>
2018-05-03 12:53:47 +0200

    Changed implementation for add functions according to the new DBConnector

--- src/main/java/com/codecool/shop/dao/implementation/CartItemDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/CartItemDaoJdbc.java
@@ -3,28 +3,36 @@ package com.codecool.shop.dao.implementation;
 import com.codecool.shop.dao.CartItemDAO;
 import com.codecool.shop.model.Product;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CartItemDaoJdbc implements CartItemDAO {
 
     @Override
     public void add(Product item, int orderId) {
+        orderId = 1;
+        int productId = item.getId();
+        try (Connection connection = DBConnector.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO cart_items (product_id, order_id, quantity)" +
+                            "VALUES (?,?,?)" +
+                            "ON CONFLICT (product_id, order_id) DO UPDATE" +
+                            "SET quantitiy = cart_items.quantitiy + 1;"
+            );
+            statement.setInt(1, productId);
+            statement.setInt(2, orderId);
+            statement.setInt(3, 1);
 
-        String query = "INSERT INTO cart_items (product_id, order_id)" +
-                        "Values (?, ?)";
-        String[] params = {Integer.toString(item.getId()), "2"};
-
-        DBConnector.getInstance().executeQuery(query, params);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void remove(Product item, int orderId) {
-        String query = "INSERT INTO cart_items (product_id, order_id)" +
-                "Values (?, ?)";
-        String[] params = {Integer.toString(item.getId()), "2"};
-
-        DBConnector.getInstance().executeQuery(query, params);
     }
 
     @Override
