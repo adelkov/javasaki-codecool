cb3fc936b30a64d9ee0a52b2fb323bd471d8f0e0
Alex <lisztes.alex@gmail.com>
2018-05-03 15:46:45 +0200

    Testing Product Category DB version

--- src/tests/com/codecool/shop/dao/implementation/ProductCategoryDaoMemJdbcTest.java
+++ src/tests/com/codecool/shop/dao/implementation/ProductCategoryDaoMemJdbcTest.java
@@ -1,7 +1,38 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductCategoryDaoMemJdbcTest {
 
+    private ProductCategoryDaoMemJdbc productCategories;
+
+    @BeforeEach
+    void initProductCategories() {
+        productCategories = ProductCategoryDaoMemJdbc.getInstance();
+    }
+
+    @Test
+    void testAddingProductCategory() {
+        int rowsBefore = productCategories.getAll().size();
+        productCategories.add(new ProductCategory("Test", "Test", "Testing purpose"));
+        int rowsAfter = productCategories.getAll().size();
+        assertEquals(1, rowsAfter - rowsBefore);
+    }
+
+    @Test
+    void testRemovingProductCategory() {
+        productCategories.add(new ProductCategory("Test", "Test", "Test"));
+        int rowsBefore = productCategories.getAll().size();
+        int lastID = productCategories.getAll().get(rowsBefore - 1).getId();
+        productCategories.remove(lastID);
+        int rowsAfter = productCategories.getAll().size();
+
+        assertEquals(1, rowsBefore - rowsAfter);
+    }
 }
