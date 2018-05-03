0b5ca9bcb9d8658e510fc8abcdeee0eb8ccc412d
peterBarazutti <barazutti.peter@gmail.com>
2018-05-03 13:54:23 +0200

    Implemented sql queries

--- src/main/java/com/codecool/shop/dao/CartItemDAO.java
+++ src/main/java/com/codecool/shop/dao/CartItemDAO.java
@@ -3,11 +3,12 @@ package com.codecool.shop.dao;
 import com.codecool.shop.model.Product;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CartItemDAO {
 
     void add(Product item, int orderId);
     void remove(Product item, int orderId);
 
-    List<Product> getAllByOrder(Integer orderId);
+    Map<Integer, Integer> getAllByOrder(Integer orderId);
 }
--- src/main/java/com/codecool/shop/dao/implementation/CartItemDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/CartItemDaoJdbc.java
@@ -5,8 +5,12 @@ import com.codecool.shop.model.Product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CartItemDaoJdbc implements CartItemDAO {
 
@@ -14,7 +18,7 @@ public class CartItemDaoJdbc implements CartItemDAO {
     public void add(Product item, int orderId) {
         orderId = 1;
         int productId = item.getId();
-        try (Connection connection = DBConnector.getConnection();) {
+        try (Connection connection = DBConnector.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(
                     "INSERT INTO cart_items (product_id, order_id, quantity)" +
                             "VALUES (?,?,?)" +
@@ -25,6 +29,8 @@ public class CartItemDaoJdbc implements CartItemDAO {
             statement.setInt(2, orderId);
             statement.setInt(3, 1);
 
+            statement.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -33,10 +39,50 @@ public class CartItemDaoJdbc implements CartItemDAO {
 
     @Override
     public void remove(Product item, int orderId) {
+        int productId = item.getId();
+        orderId = 1;
+        try (Connection connection = DBConnector.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM cart_items WHERE product_id = ? AND order_id = ? AND quantity = 1;"
+            );
+            statement.setInt(1, productId);
+            statement.setInt(2, orderId);
+
+            statement.executeUpdate();
+
+            PreparedStatement statement2 = connection.prepareStatement(
+                    "UPDATE cart_items SET quantity = cart_items.quantity - 1 " +
+                            "WHERE product_id = ? AND order_id = ?;"
+            );
+            statement2.setInt(1, productId);
+            statement2.setInt(2, orderId);
+
+            statement2.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public List<Product> getAllByOrder(Integer orderId) {
-        return null;
+    public Map<Integer, Integer> getAllByOrder(Integer orderId) {
+        orderId = 1;
+        Map<Integer, Integer> items = new HashMap<>();
+
+        try (Connection connection = DBConnector.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM cart_items WHERE order_id = ?;"
+            );
+            statement.setInt(1, orderId);
+
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()){
+                items.put(rs.getInt("product_id"), rs.getInt("quantity"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return items;
     }
 }
